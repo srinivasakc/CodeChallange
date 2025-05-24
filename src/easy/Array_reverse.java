a597515ced99a3682593be3354378befa58981af
srinivasakc <mail.srinivasakc@gmail.com>
2025-05-24 19:50:38 -0400

    updated code

--- src/easy/Array_reverse.java
+++ src/easy/Array_reverse.java
@@ -3,31 +3,34 @@ package easy;
 import java.util.Arrays;
 import java.util.Collections;
 
+//this will work for unsorted no duplicate array
+//if we have duplicate array can need to sorted to work
 public class Array_reverse {
-
 	public static void main(String[] args) {
-		int[] inputArray = new int[] { 5, 4, 3, 2, 1 };
-		System.out.println(Arrays.toString(revArr(inputArray)));
-		reverse(inputArray);
+		int[] inputArray = new int[] { 3, 6, 9, 8, 10, 3 };
+		Arrays.sort(inputArray);
+		System.out.println(Arrays.toString(reverseArray(inputArray)));
+//reverse the string using collections
+		rev(inputArray);
 	}
 
-	public static int[] revArr(int[] inputArray) {
-
+	public static int[] reverseArray(int[] inputArray) {
 		int start = 0;
 		int end = inputArray.length - 1;
 		while (start < end) {
-			int temp = inputArray[start];
-			inputArray[start] = inputArray[end];
-			inputArray[end] = temp;
+			int temp = start;
+			start = inputArray[end];
+			end = temp;
 			start++;
 			end--;
 		}
-		return inputArray;
-	}
 
-	static void reverse(int inputArray[]) {
+		return inputArray;
+	};
+
+	public static void rev(int[] inputArray) {
 		Collections.reverse(Arrays.asList(inputArray));
-		System.out.println(Arrays.toString(inputArray));
+		System.out.println(Arrays.toString(reverseArray(inputArray)));
 	}
 
 }
