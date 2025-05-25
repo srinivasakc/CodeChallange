6194683a2c698984274c7acf291eeebc49ee4b27
srinivasakc <mail.srinivasakc@gmail.com>
2025-05-24 21:01:53 -0400

    renamed the file

--- src/medium/countAndPrint_Primes.java
+++ src/medium/countAndPrint_Primes.java
@@ -2,7 +2,7 @@ package medium;
 
 import java.util.Arrays;
 
-public class Count_Primes {
+public class countAndPrint_Primes {
 
 	public static void main(String[] args) {
 		System.out.println(countAndPrintPrimes(10));
