0734a63568016cbac47472534519a0079b279f7a
srinivasakc <mail.srinivasakc@gmail.com>
2025-06-02 11:43:58 -0400

    added comments

--- src/easy/PermutationOfString.java
+++ src/easy/PermutationOfString.java
@@ -9,16 +9,18 @@ public class PermutationOfString {
 	}
 
 	private static void printPermutation(String str, String result) {
-		// TODO Auto-generated method stub
-
+		// If string is empty
 		if (str.length() == 0) {
 			System.out.println(result);// print the results
 			return;
 		}
 
 		for (int i = 0; i < str.length(); i++) {
+			// ith character of str
 			char c = str.charAt(i);
+			// Rest of the string after excluding the ith character
 			String reset = str.substring(0, i) + str.substring(i + 1);
+			// Recursive call
 			printPermutation(reset, result + c);
 
 		}
