a54204b9d971f6a6e4b4324c1db582a92c3ed112
srinivasakc <mail.srinivasakc@gmail.com>
2025-06-02 12:31:38 -0400

    commiting code

--- src/easy/Power_Of_Two_Number.java
+++ src/easy/Power_Of_Two_Number.java
@@ -3,25 +3,20 @@ package easy;
 public class Power_Of_Two_Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		if (isPowerOfTwo(31))
-			System.out.println("Yes");
-		else
-			System.out.println("No");
+		System.out.println(isPowerOfTwo(-3));
+		System.out.println(isPowerOfTwo(256));
+		System.out.println(isPowerOfTwo(65));
+		System.out.println(isPowerOfTwo(1));
 
-		if (isPowerOfTwo(64))
-			System.out.println("Yes");
-		else
-			System.out.println("No");
 	}
 
+	// check n is power of 2
 	private static boolean isPowerOfTwo(int n) {
-		// TODO Auto-generated method stub
 
 		if (n == 0) {
-			return false;
+			return false;// when n equals zero return false
 		}
-		while (n > 1) {
+		while (n != 1) {// when n not equal to 1, divide the number by 2
 			if (n % 2 != 0) {
 				return false;
 			}
