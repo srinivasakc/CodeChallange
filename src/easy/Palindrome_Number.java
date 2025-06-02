c55325323fd005af747eced0d074af35799105dc
srinivasakc <mail.srinivasakc@gmail.com>
2025-06-02 11:18:58 -0400

    added comments

--- src/easy/Palindrome_Number.java
+++ src/easy/Palindrome_Number.java
@@ -19,24 +19,26 @@ package easy;
 public class Palindrome_Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(isPalindrome(1));
 		System.out.println(isPalindrome(121));
 		System.out.println(isPalindrome(2442));
 	}
 
 	private static boolean isPalindrome(int num) {
-		// TODO Auto-generated method stub
+		// keep num in temp, initialise reverse and remainder to zero
 		int rev = 0, reminder = 0, temp = num;
-
+		// number less than zero or (num divided by 10 wil no remainder and not equal to
+		// zero)
 		if (num < 0 || (num % 10 == 0 && num != 0)) {// num<0 and single digit like 1, 2,4,5....
 			return false;
 		}
+		// reverse the number
 		while (num > 0) {
 			reminder = num % 10;
 			rev = rev * 10 + reminder;
 			num = num / 10;
 		}
+		// palindrome if original number is equal to reversed number
 		if (temp == rev) {
 			return true;
 		} else {
