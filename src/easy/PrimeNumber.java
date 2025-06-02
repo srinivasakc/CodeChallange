c69f24d13b4b510255e9e2c9314741f1ac52f96b
srinivasakc <mail.srinivasakc@gmail.com>
2025-06-02 14:22:18 -0400

    commited code

--- src/easy/PrimeNumber.java
+++ src/easy/PrimeNumber.java
@@ -1,12 +1,13 @@
 package easy;
 
+//Prime numbers are natural numbers that are divisible by only 1 and the number itself
+//The number 1 is not a prime number because it only has one positive divisor (itself), while prime numbers are defined as having exactly two distinct positive divisors
 public class PrimeNumber {
 
 	public static void main(String[] args) {
 		System.out.println(isPrime(29)); // true
 		System.out.println(isPrime(35)); // false
 		System.out.println(isPrime(20)); // false
-		System.out.println(isPrime(27)); // false
 		System.out.println(isPrime(7)); // true
 
 	}
