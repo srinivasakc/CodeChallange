adc13f5c94faffe7ce6f9864836b962f8d5d0ac0
srinivasakc <mail.srinivasakc@gmail.com>
2025-06-12 20:22:08 -0400

    CountVowelsConsonants

---
+++ src/easy/Count_Vowels_Consonants.java
@@ -0,0 +1,34 @@
+package easy;
+
+public class Count_Vowels_Consonants {
+
+	public static void main(String[] args) {
+
+		String str = "Automation";
+
+		int[] count = CountVowelsConsonants(str);
+		System.out.println("Vowels count: " + count[0]);
+		System.out.println("consonants count: " + count[1]);
+
+	}
+
+	public static int[] CountVowelsConsonants(String str) {
+		int vowelsCount = 0;
+		int consonantsCount = 0;
+		String vowels = "aeiouAEIOU";
+
+		for (char c : str.toCharArray()) {
+			if (vowels.indexOf(c) != -1) {
+				vowelsCount++;
+				System.out.println("Vowel- " + c);
+			} else if (Character.isLetter(c)) {
+				System.out.println("Consonants- " + c);
+				consonantsCount++;
+			}
+
+		}
+
+		return new int[] { vowelsCount, consonantsCount };
+	}
+
+}
