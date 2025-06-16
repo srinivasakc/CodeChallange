69791f6e79e54663914f3e36dbe0eaf37632dd7a
srinivasakc <mail.srinivasakc@gmail.com>
2025-06-16 12:26:09 -0400

    updated

--- src/easy/FindAllSubString.java
+++ src/easy/FindAllSubString.java
@@ -5,14 +5,13 @@ public class FindAllSubString {
 	public static void main(String[] args) {
 		String str = "abc";
 		findAllSubstring(str);
-
 	}
 
 	public static void findAllSubstring(String str) {
 
 		for (int i = 0; i < str.length(); i++) {
 			for (int j = i + 1; j <= str.length(); j++) {
-				System.out.println(str.substring(i, j));
+				System.out.print(str.substring(i, j) + " ");
 			}
 
 		}
