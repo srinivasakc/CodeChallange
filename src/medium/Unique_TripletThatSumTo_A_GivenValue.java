d244b8213bf0eaa829f1ae2563346ef50c1cc5dc
srinivasakc <mail.srinivasakc@gmail.com>
2025-06-16 10:53:37 -0400

    added new code

---
+++ src/medium/Unique_TripletThatSumTo_A_GivenValue.java
@@ -0,0 +1,79 @@
+package medium;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+
+//All unique triplets that sum up to a given value
+public class Unique_TripletThatSumTo_A_GivenValue {
+
+	// returns all triplets whose sum is
+	// equal to sum value
+	public static List<List<Integer>> findTriplets(int[] nums, int sum) {
+
+		/* Sort the elements */
+		Arrays.sort(nums);
+
+		List<List<Integer>> pair = new ArrayList<>();
+		List<Integer> triplets = new ArrayList<>();
+		TreeSet<String> set = new TreeSet<String>();
+
+		/*
+		 * Iterate over the array from the start and consider it as the first element
+		 */
+		for (int i = 0; i < nums.length - 2; i++) {
+
+			// index of the first element in the
+			// remaining elements
+			int j = i + 1;
+
+			// index of the last element
+			int k = nums.length - 1;
+
+			while (j < k) {
+
+				if (nums[i] + nums[j] + nums[k] == sum) {
+
+					String str = nums[i] + ":" + nums[j] + ":" + nums[k];
+
+					if (!set.contains(str)) {
+
+						// To check for the unique triplet
+						triplets.add(nums[i]);
+						triplets.add(nums[j]);
+						triplets.add(nums[k]);
+						pair.add(triplets);
+						triplets = new ArrayList<>();
+						set.add(str);
+					}
+
+					// increment the second value index
+					j++;
+
+					// decrement the third value index
+					k--;
+				} else if (nums[i] + nums[j] + nums[k] < sum)
+					j++;
+
+				else
+					k--;
+			}
+		}
+		return pair;
+	}
+
+	// Driver code
+	public static void main(String[] args) {
+		int[] nums = { 12, 3, 6, 1, 6, 9 };
+		int sum = 24;
+
+		List<List<Integer>> triplets = findTriplets(nums, sum);
+
+		// Function call
+		if (!triplets.isEmpty()) {
+			System.out.println(triplets);
+		} else {
+			System.out.println("No triplets can be formed");
+		}
+	}
+}
